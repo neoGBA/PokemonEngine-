2d28092e8ea42e459a957aa31b12dcb1d3a4b010
jimbabwe216 <31488448+jimbabwe216@users.noreply.github.com>
2018-03-20 08:57:30 -0600

    Pokemon 1-18 added

---
+++ src/controller/Battle.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class Battle {
+	
+	public Battle() {
+		startBattle();
+	}
+	
+	public void startBattle() {
+		
+	}
+	
+	public void inBattle() {
+		
+	}
+	
+	public void endBattle() {
+		
+	}
+	
+}
